542589b49030f0aff59f49079ba8b24699d9a3aa
DominykMcNamara <dominyksmith@gmail.com>
2022-11-19 13:28:10 -0500

    typography config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        black: 'rgb(21, 21, 21)',
+        neonGreen: 'rgb(78, 225, 160)',
+        lightBlack: 'rgb(36, 36, 36)',
+        gray: 'rgb(217, 217, 217)',
+        white: 'rgb(255, 255, 255)'
+      }
+    },
+    fontFamily: {
+      base: [ 'Space Grotesk', 'sans-serif', 'ui-sans-serif', "system-ui"],
+    },
+    letterSpacing: {
+      h1: '-0.156rem',
+      h2: '-0.094rem'
+    },
+    fontSize: {
+      h1: ["5.5rem", { lineHeight: "5.5rem"}],
+      h2: ["3rem", { lineHeight: "3.5rem" }],
+      h3: ["1.5rem", { lineHeight: "2rem" }],
+      p: ["1.125rem", { lineHeight: "1.75rem" }]
+    },
     extend: {},
   },
   plugins: [],
